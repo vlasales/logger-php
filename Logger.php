97133e1ab597e9afa237547d51c93cb6c2e541a7
Vlastimil Pospíchal <vlastimil.pospichal@gmail.com>
2020-08-14 17:30:39 +0200

    add assert

--- Logger.php
+++ Logger.php
@@ -6,6 +6,9 @@ class Logger implements LoggerInterface {
     }
 
     public function __call(string $level, array $args) {
+        assert(in_array($level, [info', 'notice', 'warning', 'error', 'critical']));
+        assert(gettype($args[0]) === "string");
+        assert(gettype($args[1]) === "string");
         $this->log($level, $args[0], $args[1]);
     }
 }
